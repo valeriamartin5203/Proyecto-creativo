01a8fc1f224266e6a388de6b3277fff6876cb34e
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-02-12 21:34:48 -0600

    agregar para subir a reader

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto", PORT);
+});
+
 app.use(express.static("public"));
 
 const players = {};
