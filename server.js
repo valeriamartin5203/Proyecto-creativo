7d601aedd7818523558ccdf4c6620d2d205877f4
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-02-12 21:44:03 -0600

    arreglando errores para subirlo a render

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto", PORT);
-});
-
 app.use(express.static("public"));
 
 const players = {};
@@ -30,9 +26,7 @@ function getPlayersInRoom(room) {
 io.on("connection", (socket) => {
   console.log("🟢 Usuario conectado:", socket.id);
 
-  // 🔥 JOIN CORREGIDO (ahora recibe objeto)
   socket.on("joinGame", (data) => {
-
     players[socket.id] = {
       id: socket.id,
       name: data.username,
@@ -59,10 +53,7 @@ io.on("connection", (socket) => {
 
     const room = players[socket.id].room;
 
-    io.to(room).emit(
-      "playersUpdate",
-      getPlayersInRoom(room)
-    );
+    io.to(room).emit("playersUpdate", getPlayersInRoom(room));
   });
 
   socket.on("changeRoom", (newRoom) => {
@@ -75,17 +66,8 @@ io.on("connection", (socket) => {
 
     players[socket.id].room = newRoom;
 
-    // Actualizar sala vieja
-    io.to(oldRoom).emit(
-      "playersUpdate",
-      getPlayersInRoom(oldRoom)
-    );
-
-    // Actualizar sala nueva
-    io.to(newRoom).emit(
-      "playersUpdate",
-      getPlayersInRoom(newRoom)
-    );
+    io.to(oldRoom).emit("playersUpdate", getPlayersInRoom(oldRoom));
+    io.to(newRoom).emit("playersUpdate", getPlayersInRoom(newRoom));
   });
 
   socket.on("mensaje", (text) => {
@@ -107,15 +89,12 @@ io.on("connection", (socket) => {
 
     delete players[socket.id];
 
-    io.to(room).emit(
-      "playersUpdate",
-      getPlayersInRoom(room)
-    );
+    io.to(room).emit("playersUpdate", getPlayersInRoom(room));
 
     console.log("🔴 Usuario desconectado:", socket.id);
   });
 });
 
-server.listen(3000, () =>
-  console.log("Servidor corriendo en http://localhost:3000")
-);
+server.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
+});
