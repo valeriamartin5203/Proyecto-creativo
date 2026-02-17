30fdb05896be0c99a0dec6e227920d71fac5372a
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-02-17 11:02:23 -0600

    arreglando error

--- public/game.js
+++ public/game.js
@@ -5,7 +5,20 @@ const socket = io();
 const username = prompt("Ingresa tu nombre:") || "Invitado";
 
 
+// ==========================================
+// 📦 CARGAR DATOS GUARDADOS
+// ==========================================
+let savedData = JSON.parse(localStorage.getItem("playerData"));
 
+let myPosition = savedData ? { x: savedData.x, y: savedData.y } : { x: 375, y: 225 };
+let currentDirection = savedData ? savedData.direction : "down";
+let currentRoom = savedData ? savedData.room : "plaza";
+
+const game = document.getElementById("game");
+const mensajes = document.getElementById("mensajes");
+const texto = document.getElementById("texto");
+
+game.classList.add(currentRoom);
 
 
 // ==========================================
